8d2ebff530a1a206709b9cacc7baf3de3667ec9b
Drew Ulmer <drew@unabridgedsoftware.com>
2023-08-21 09:20:14 -0500

    Add spec for params with multiple top level keys

--- test/auto_strong_parameters_test.rb
+++ test/auto_strong_parameters_test.rb
@@ -13,4 +13,45 @@ class AutoStrongParametersTest < Minitest::Test
     assert_equal ["name", {"pet" => ["name"]}], to_strong_params_shape({"name" => "Steve", "pet" => { "name" => "Fluffy" }})
     assert_equal ["name", {"pet" => ["name"]}], to_strong_params_shape({"name" => "Steve", "pet" => { "name" => "Fluffy" }})
   end
+
+  def test_nested_params
+    params = {
+      "user"=>{
+        "first_name"=>"Steve",
+        "last_name"=>"Sample",
+        "email"=>"steve@example.com",
+        "phone"=>"(123) 234-2345",
+        "work_location"=>"Chicago",
+        "birth_date"=>"1980-01-01",
+      },
+      "address"=>{
+        "id"=>"7",
+        "street_address"=>"123 Example St",
+        "city"=>"Chicago",
+        "state"=>"IL",
+        "zip"=>"12345"
+      },
+      "emergency_contact"=>{
+        "id"=>"16",
+        "first_name"=>"Mary",
+        "last_name"=>"Example",
+        "phone"=>"(123) 123-1234",
+      },
+    }
+    exp = {
+      "user"=>[
+        "first_name", "last_name", "email", "phone", "work_location", "birth_date"
+      ],
+      "address"=>[
+        "id", "street_address", "city", "state", "zip"
+      ],
+      "emergency_contact"=>[
+        "id", "first_name", "last_name", "phone"
+      ],
+    }
+    actual = AutoStrongParameters.to_strong_params_shape(params)
+    assert_equal exp["user"], actual["user"]
+    assert_equal exp["address"], actual["address"]
+    assert_equal exp["emergency_contact"], actual["emergency_contact"]
+  end
 end
